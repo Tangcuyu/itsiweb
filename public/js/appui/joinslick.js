b2bb8ceb9f7aae7794bf0db1f02db39121752b52
tangcuyu <joysmshr@126.com>
2017-09-22 17:42:27 +0800

    joinus update

---
+++ public/js/appui/joinslick.js
@@ -0,0 +1,13 @@
+$('.single-item').slick({
+  
+  accessibility: true,
+  dots: true,
+  infinite: true,
+  arrows: true,
+  speed: 800,
+  slidesToShow: 1,
+  autoplay: true,
+  fade:true,
+  autoplaySpeed: 5000,
+  slidesToScroll: 1
+});
