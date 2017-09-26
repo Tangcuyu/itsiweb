7d723d81a1587d99f3ae0cfd5bf88cd4f675f292
tangcuyu <joysmshr@126.com>
2017-09-26 19:15:01 +0800

    add go to top

---
+++ public/js/appui/gotoTop.js
@@ -0,0 +1,16 @@
+ $(window).scroll(function(){
+        var sc=$(window).scrollTop();
+        var rwidth=$(window).width()+$(document).scrollLeft();
+        var rheight=$(window).height()+$(document).scrollTop();
+        if(sc>0){
+            $("#goTop").css("display","block");
+            $("#goTop").css("left",(rwidth-80)+"px");
+            $("#goTop").css("top",(rheight-120)+"px");
+        }else{
+            $("#goTop").css("display","none");
+        }
+    });
+    
+$("#goTop").click(function(){
+        $('body,html').animate({scrollTop:0},300);
+    });
